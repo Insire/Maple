930583b3c0e378239416256c4c1812c9b53c5fbc
Ilya Sosnovsky <yakrewedko@ya.ru>
2017-11-01 12:34:24 +0300

    Fix V3025
    Incorrect format. A different number of format items is expected while calling 'Format' function. Arguments not used: callerName. Maple.Core GenericExtensions.cs 12

--- src/Maple.Core/Extensions/GenericExtensions.cs
+++ src/Maple.Core/Extensions/GenericExtensions.cs
@@ -9,7 +9,7 @@ namespace Maple.Core.Extensions
         public static T ThrowIfNull<T>(this T obj, string objName, [CallerMemberName]string callerName = null)
         {
             if (obj == null)
-                throw new ArgumentNullException(objName, string.Format("{0} {1} (2)", objName, Resources.IsRequired, callerName));
+                throw new ArgumentNullException(objName, string.Format("{0} {1} {2}", objName, Resources.IsRequired, callerName));
 
             return obj;
         }
